e6cadb098afc7577c74ebfde5ee0c15ecec9a229
Brian Kurilko <briankurilko@gmail.com>
2024-04-16 20:58:06 -0400

    Used new script that now also tests

--- tests/submissions.sh
+++ tests/submissions.sh
@@ -11,7 +11,7 @@ if ! gcloud pubsub subscriptions create "$subscriptionName" --topic "$topicName"
     echo "Subscription $subscriptionName already exists"
 fi
 
-gcloud builds submit . --config cloudbuild.yaml --substitutions "_GIT_CLONE_URL=https://github.com/brianpipeline/test-node.git,_GIT_REPOSITORY_NAME="test-node",_GIT_REF="refs/heads/main",_GIT_HEAD_SHA="abc034383273c99877a5ce5c08269cb714cd3d40",_REPLY_TOPIC=\"projects/cloud-build-pipeline-396819/topics/$topicName\"" --region=us-central1
+gcloud builds submit . --config cloudbuild.yaml --substitutions "_GIT_CLONE_URL=https://github.com/brianpipeline/test-node.git,_GIT_REPOSITORY_NAME="test-node",_GIT_REF="refs/heads/main",_GIT_HEAD_SHA="8f15568532099dba3a04eb1b74859f03046e7bca",_REPLY_TOPIC=\"projects/cloud-build-pipeline-396819/topics/$topicName\"" --region=us-central1
 
 message=$(gcloud pubsub subscriptions pull --auto-ack "$subscriptionName" --format='value(message.data)' 2>/dev/null)
 gcloud pubsub topics delete "$topicName"
